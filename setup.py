1871af8911acb9b7f0c370ec8ec3ea27ba7e9a62
芮峰云 <rfyiamcool@163.com>
2015-09-13 11:02:11 +0800

    modify setup.py info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
-version = '1.0.0'
+version = '1.1.0'
 
 install_requires = [
     'python-etcd'
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(name='pyetcdlock',
     version=version,
-    description="python etcd network mutux lock",
+    description="python etcd network mutux lock,support watch ,force ,incr lock time",
     long_description=README,
     classifiers = [
          'Development Status :: 2 - Pre-Alpha',
@@ -21,12 +21,13 @@ setup(name='pyetcdlock',
          'Programming Language :: Python :: 2.7',
          'Programming Language :: Python :: 3.0',
          'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
-    keywords= ['etcd lock mutex','fengyun'],
+    ],
+    keywords = ['mutux network lock based on etcd','fengyun'],
     author='ruifengyun',
     author_email='rfyiamcool@163.com',
     url='https://github.com/rfyiamcool',
     license='MIT',
     packages=['pyetcdlock'],
+    long_description = read('README.md'),
     install_requires=install_requires,
 )
